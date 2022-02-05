43a488c139a3c02c8c07bf84cee0e32035fc5a6d
swetha naidu <swethanaidu@users.noreply.github.com>
2022-02-05 21:05:41 +0530

    updated app.js for heroku prod

--- server/app.js
+++ server/app.js
@@ -47,7 +47,7 @@ mongoose.connect(
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
